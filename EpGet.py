b01a64cdbd4af44a39368b7246d41c9539f70609
Mottelz <shachna@gmail.com>
2018-04-13 02:24:36 -0400

    Set up the methods to grab season info. Still need to add file renaming part.

--- EpGet.py
+++ EpGet.py
@@ -1,10 +1,19 @@
 import requests
 import json
 
-headers = {'Content-Type': 'application/json','Accept': 'application/json',}
+def getToken():
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json', }
+    data = '{  "apikey": "B4AAF3CDC8191A8A",  "userkey": "9E37DA78322F6E6A",  "username": "mottelz"}'
+    response = requests.post('https://api.thetvdb.com/login', headers=headers, data=data)
+    token = json.loads(response.content)['token']
+    return token
 
-data = '{  "apikey": "B4AAF3CDC8191A8A",  "userkey": "9E37DA78322F6E6A",  "username": "mottelz"}'
 
-response = requests.post('https://api.thetvdb.com/login', headers=headers, data=data)
+def getSeasonData(token, showid, season):
+    headers = {'Accept': 'application/json','Authorization': 'Bearer '+token,}
+    response = requests.get('https://api.thetvdb.com/series/'+showid+'/episodes/query?airedSeason='+season, headers=headers)
+    return json.loads(response.content)['data']
 
-token = json.loads(response.content)
+
+token = getToken()
+print(getSeasonData(token, '277928', '4'))
